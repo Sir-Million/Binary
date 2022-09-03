2972f6905abc98353a5bde157a945096c754148d
Sir_Milllion <73513663+Sir-Million@users.noreply.github.com>
2022-09-02 20:00:57 -0500

    Conversión inversa añadida

--- index.js
+++ index.js
@@ -1,24 +1,90 @@
 const entrada = document.getElementById('entrada');
 const binario = document.getElementById('Binario');
+const binario2 = document.getElementById('Binario2');
 const octal = document.getElementById('Octal');
+const octal2 = document.getElementById('Octal2');
 const salida = document.getElementById('salida');
 
 let numeroDecimal, divisor, resultadoBinario, cociente, residuo;
 
 binario.addEventListener('click',()=>{
-    validarEntrada();
-    resultadoBinario = convertirDecimalABinario();
-    salida.value = invertirCadena(resultadoBinario);
+    validarEntrada1();
 })
 
 octal.addEventListener('click',()=>{
-    validarEntrada();
-    resultadoOctal = convertirDecimalAOctal();
-    salida.value = invertirCadena(resultadoOctal);
+    validarEntrada2();
 })
 
-const validarEntrada = () => {
-    (entrada.value === "") ? alert('Porfavor, ingresa un numero decimal para poder convertirlo a uno binario o octal.') : console.log('Todo bien');
+binario2.addEventListener('click',()=>{
+    validarEntrada3();
+})
+
+octal2.addEventListener('click',()=>{
+    validarEntrada4();
+})
+
+const validarEntrada1 = () => {
+    if (!isNaN(entrada.value) === false || entrada.value === "") {
+        alert('Error: asegurate de que el numero de entrada este bien definido y sea un caracter numerico');
+        return console.log("numero no definido");
+    } else {
+        console.log('success');
+    }
+    resultadoBinario = convertirDecimalABinario();
+    salida.value = invertirCadena(resultadoBinario);
+}
+
+const validarEntrada2 = () => {
+    if (!isNaN(entrada.value) === false || entrada.value === "") {
+        alert('Error: asegurate de que el numero de entrada este bien definido y sea un caracter numerico');
+        return console.log("numero no definido");
+    } else {
+        console.log('success');
+        resultadoOctal = convertirDecimalAOctal();
+        salida.value = invertirCadena(resultadoOctal);
+    }
+}
+
+const validarEntrada3 = () => {
+    if (!isNaN(entrada.value) === false || entrada.value === "") {
+        alert('Error: asegurate de que el numero de entrada este bien definido y sea un caracter numerico');
+        return console.log("numero no definido");
+    } else {
+        console.log('success');
+        resultadoBinario2 = convertirBiniarioADecimal(entrada.value);
+        salida.value = resultadoBinario2;
+    }
+}
+
+function convertirBiniarioADecimal(binarioval) {
+    if (binarioval.constructor != String) {
+        return null;
+    }
+
+    binarioval = binarioval.replace(/[^01]/gi, '');
+
+    return Number.parseInt(binarioval, 2);
+}
+
+const validarEntrada4 = () => {
+    if (!isNaN(entrada.value) === false || entrada.value === "") {
+        alert('Error: asegurate de que el numero de entrada este bien definido y sea un caracter numerico');
+        return console.log("numero no definido");
+    } else {
+        console.log('success');
+        resultadoOctal2 = convertirOctalADecimal(entrada.value);
+        salida.value = resultadoOctal2;
+    }
+}
+
+function convertirOctalADecimal(octalval) {
+    if (octalval.constructor != String) {
+        return null;
+    }
+
+    octalval = octalval.replace(/[^01234567]/gi, '');
+
+    return Number.parseInt(octalval, 8);
 }
 
 const convertirDecimalABinario = () => {
@@ -55,5 +121,4 @@ const convertirDecimalAOctal = () => {
 
 const invertirCadena = (cadenaTexto) => {
     return cadenaTexto.split("").reverse().join("");
-    
 }
